afd13a862dc07485c0f619a3e39521f7ce6fc65e
texttechne <gedankengut@gmx.de>
2023-08-30 03:32:51 +0200

    feat(jquery): allow for query params

--- packages/jquery/src/AjaxRequestConfig.ts
+++ packages/jquery/src/AjaxRequestConfig.ts
@@ -8,16 +8,21 @@ const DEFAULT_CONFIG: JQuery.AjaxSettings = {
 /**
  * Available config options for end user when making a given request.
  */
-export type AjaxRequestConfig = Pick<
-  JQuery.AjaxSettings,
-  "complete" | "beforeSend" | "headers" | "statusCode" | "timeout"
->;
+export interface AjaxRequestConfig
+  extends Pick<JQuery.AjaxSettings, "complete" | "beforeSend" | "headers" | "statusCode" | "timeout"> {
+  /**
+   * Add query params.
+   */
+  params?: Record<string, string | number | boolean | Array<string | number | boolean>>;
+}
 
-export function getDefaultConfig(config?: AjaxRequestConfig): JQuery.AjaxSettings {
+export interface InternalRequestConfig extends JQuery.AjaxSettings, Pick<AjaxRequestConfig, "params"> {}
+
+export function getDefaultConfig(config?: AjaxRequestConfig): InternalRequestConfig {
   return mergeAjaxConfig(DEFAULT_CONFIG, config);
 }
 
-export function mergeAjaxConfig(config?: JQuery.AjaxSettings, toMerge?: JQuery.AjaxSettings) {
+export function mergeAjaxConfig(config?: JQuery.AjaxSettings, toMerge?: JQuery.AjaxSettings): InternalRequestConfig {
   const { headers, ...passThrough } = config || {};
   const { headers: headers2, ...passThrough2 } = toMerge || {};
   return {
